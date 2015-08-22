b3ca477d7a2263f82adf0cb6056bb33bf62a55bc
gvpavlov <georgipavlov94@gmail.com>
2015-08-22 20:21:58 +0300

    Add background and .gitignore

--- gui/aquarium.py
+++ gui/aquarium.py
@@ -8,7 +8,7 @@ from ui_aquarium import Ui_aquarium
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
 parent_directory = os.path.abspath(os.path.join(current_directory, os.pardir))
-resources_directory = os.path.join(current_directory, "resources")
+resource_directory = os.path.join(current_directory, "resources")
 sys.path.append(os.path.join(parent_directory, "core"))
 
 from game import Game
@@ -29,6 +29,7 @@ class Aquarium(QMainWindow):
         canvas.begin(self)
         #canvas.scale(self.size().width(), self.size().height())
         canvas.setPen(QtCore.Qt.NoPen)
+        canvas.drawPixmap(0, 0, self.background.scaled(1000, 700))
         self.draw_alien(canvas)
 
     def keyPressEvent(self, event):
@@ -55,7 +56,7 @@ class Aquarium(QMainWindow):
 
     def draw_alien(self, canvas):
         self.alien_image = QtGui.QPixmap(os.path
-            .join(resources_directory, "alien.png"))
+            .join(resource_directory, "alien.png"))
         canvas.drawPixmap(self.game.alien.x, self.game.alien.y,
                           self.alien_image, self.alien_frameX,
                           self.alien_frameY, 160, 160)
@@ -66,3 +67,5 @@ class Aquarium(QMainWindow):
         self.previous_direction = (-1, 0)
         self.alien_frameX = 0
         self.alien_frameY = 0
+        self.background = QtGui.QPixmap(
+            os.path.join(resource_directory, "background.png"))
